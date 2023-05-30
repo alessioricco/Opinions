767e0ac6ef92f43ba30ee7ffcfe7696edb111d5c
alessioricco <alessio.ricco@gmail.com>
2023-05-30 19:42:34 +0100

    release 0.0.0.1

--- content.js
+++ content.js
@@ -1,11 +1,8 @@
-// content.js
-
-// Event listener for mouseup event
-document.addEventListener('mouseup', function() {
-  var selectedText = window.getSelection().toString();
-  console.log(selectedText)
-  if (selectedText !== '') {
-    // Send a message to the background script
-    chrome.runtime.sendMessage({ action: 'selectedText', text: selectedText });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'getSelectedText') {
+      sendResponse({data: window.getSelection().toString()});
   }
 });
+
+
+
